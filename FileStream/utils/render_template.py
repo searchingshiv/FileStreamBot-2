f34759deace5da53514ec9afccc54a180494b735
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-19 15:44:50 +0530

    Update render_template.py

--- FileStream/utils/render_template.py
+++ FileStream/utils/render_template.py
@@ -1,25 +1,45 @@
-import aiohttp
-import aiofiles
-import urllib.parse
-from FileStream.config import Telegram, Server
-from FileStream.utils.database import Database
+from FileStream.config import Telegram
+from FileStream.bot import FileStream
 from FileStream.utils.human_readable import humanbytes
-db = Database(Telegram.DATABASE_URL, Telegram.SESSION_NAME)
+from FileStream.utils.file_properties import get_file_ids
+from FileStream.server.exceptions import InvalidHash
+import urllib.parse
+import aiofiles
+import logging
+import aiohttp
+import jinja2
 
-async def render_page(db_id):
-    file_data=await db.get_file(db_id)
-    src = urllib.parse.urljoin(Server.URL, f'dl/{file_data["_id"]}')
+async def render_page(id, secure_hash, src=None):
+    file = await StreamBot.get_messages(int(Var.BIN_CHANNEL), int(id))
+    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
+    if file_data.unique_id[:6] != secure_hash:
+        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
+        logging.debug(f"Invalid hash for message with - ID {id}")
+        raise InvalidHash
 
-    if str((file_data['mime_type']).split('/')[0].strip()) == 'video':
-        async with aiofiles.open('FileStream/template/stream.html') as r:
-            heading = 'Watch {}'.format(file_data['file_name'])
-            html_template = await r.read()
-            html = html_template.replace('streamMediaLink', src).replace('streamHeading', heading)
+    src = urllib.parse.urljoin(
+        Var.URL,
+        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
+    )
+
+    tag = file_data.mime_type.split("/")[0].strip()
+    file_size = humanbytes(file_data.file_size)
+    if tag in ["video", "audio"]:
+        template_file = "FileStream/template/req.html"
     else:
-        async with aiofiles.open('FileStream/template/dl.html') as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    heading = 'Download {}'.format(file_data['file_name'])
-                    file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    html = (await r.read()) % (heading, file_data['file_name'], src, file_size)
-    return html
+        template_file = "FileStream/template/dl.html"
+        async with aiohttp.ClientSession() as s:
+            async with s.get(src) as u:
+                file_size = humanbytes(int(u.headers.get("Content-Length")))
+
+    with open(template_file) as f:
+        template = jinja2.Template(f.read())
+
+    file_name = file_data.file_name.replace("_", " ")
+
+    return template.render(
+        file_name=file_name,
+        file_url=src,
+        file_size=file_size,
+        file_unique_id=file_data.unique_id,
+    )
